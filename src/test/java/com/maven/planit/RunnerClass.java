a465540c84d73be686115d465deb24b7f6d3d35e
HarithaMaganti <harithamaganti@gmail.com>
2019-10-21 11:13:33 +0100

    Yourlogo src

--- src/test/java/com/maven/planit/RunnerClass.java
+++ src/test/java/com/maven/planit/RunnerClass.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources", dryRun = false, strict = true, tags = "@sendMessage,@ProductSelectionAndRemoval",
-        plugin = {"json:src/test/yourlogo.json", "html:src/test/reports/cucumberReport.html"})
-      //plugin = {"json:target/Haritha.json"})
+        //plugin = {"json:src/test/yourlogo.json", "html:src/test/reports/cucumberReport.html"})
+      plugin = {"json:target/Haritha.json"})
 
 public class RunnerClass {
 }
